4be5a53d003bf3ac796504bf683eb95a61a8eae8
platff <ds.platff@gmail.com>
2021-05-28 13:31:42 +0300

    page opened

--- src/test/java/github/WikiPagesTest.java
+++ src/test/java/github/WikiPagesTest.java
@@ -1,5 +1,21 @@
 package github;
 
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class WikiPagesTest {
+
+    @Test
+    void thereShouldBeSoftAssertionsPage(){
+
+        open("https://github.com/selenide/selenide");
+
+
+    }
+
 
 }
